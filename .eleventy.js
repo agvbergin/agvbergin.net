49ac100ac7e5a13a5a99b437405c6522eb3e2032
agvbergin <agvbergin@gmail.com>
2023-01-16 10:18:18 +0000

    Move input from root to src
    Preps for installing sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,14 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
-  // Output directory: _site
-
   // Copy `img/` to `_site/img`
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.setServerPassthroughCopyBehavior("img");
+  eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.setServerPassthroughCopyBehavior("./src/img");
 
   // Copy `css/fonts/` to `_site/css/fonts`
   // Keeps the same directory structure.
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.setServerPassthroughCopyBehavior("css");
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.setServerPassthroughCopyBehavior("./src/css");
 
   // RSS plugin
   eleventyConfig.addPlugin(pluginRss);
@@ -24,4 +22,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
+
+  // Output directory: _site
+  return {
+    dir: {
+      input: "src"
+    }
+  };
 };
